84f8aafa28bab3aa2276cbaa382374c74a662257
Mundhirissa <mundhirissa48@gmail.com>
2023-02-09 10:54:08 -0800

    first commit

---
+++ src/test/java/vote/voter/FullCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package vote.voter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
